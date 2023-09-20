4e6b89459d15702474a938da3c79413ac4a2647f
LENOVO <LENOVO@DESKTOP-34RFF3K>
2023-09-19 22:22:27 -0500

    Carga inicial

---
+++ src/Clases/CLogin.java
@@ -0,0 +1,49 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Clases;
+
+import Formularios.FormMenuPrincipal;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+public class CLogin {
+     public void validaUsuario(JTextField usuario, JPasswordField contrasenia){
+    
+        try {
+            ResultSet rs=null;           
+            PreparedStatement ps= null;
+            
+            Clases.Cconexion objetoConexion = new Clases.Cconexion();
+            
+            String consulta="select * from Usuarios where Usuarios.ingresoUsuario =(?) and Usuarios.ingresoContrasenia=(?);";
+            ps=objetoConexion.estableceConexion().prepareStatement(consulta);
+            
+            String contra = String.valueOf(contrasenia.getPassword());
+            
+            ps.setString(1, usuario.getText());
+            ps.setString(2,contra);
+            
+            rs = ps.executeQuery();
+            
+            if (rs.next()) {
+                
+                JOptionPane.showMessageDialog(null,"El Usuario es Correcto");
+                FormMenuPrincipal objetoMenu = new FormMenuPrincipal();
+                objetoMenu.setVisible(true);
+            }
+            else
+            {
+                JOptionPane.showMessageDialog(null,"El Usuario es INCORRECTO, VUELVA A INTENTAR");
+            }
+            
+      
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"ERROR: "+e.toString());
+        }
+    }
+    
+}
